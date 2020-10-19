1bc28c07f574bd3a1a88dfa90d7126a74070c9ab
MarvaZy <70692128+MarvaZy@users.noreply.github.com>
2020-10-19 16:58:55 +0300

    Add files via upload

--- Search.py
+++ Search.py
@@ -13,6 +13,37 @@ class Search(db):
     def __init__(self):
         super(Search, self).__init__()
  
+    def type_error(self, list_of_words):
+        '''
+        checks for typing errors of user's input
+        output - list of words 
+        user chooses whether to change his input or not
+        '''
+        errors = list_of_words
+        correct_words = list_of_words
+        i=1
+        while i<=1:
+            records = self.select(i)
+            # casting to a list only the ingredients 
+            ingr = [i.split() for i in list(list(zip(*records)))[2]]
+            ingr_flat = [item for sublist in ingr for item in sublist]
+            for word in errors: 
+                if word in ingr_flat:
+                    print(word)
+                    errors.remove(word)
+                if len(errors) == 0:
+                    break
+            i += 1
+        # if errors list still have values, it means no match was found for them
+        if len(errors):
+            print("ייתכן והייתה שגיאה בקלט שהוזן. המילים הבאות לא נמצאו במאגרינו: ", end="")
+            print(*errors)
+            another_input = input("לשיקולך, הזן כעת את בחירותייך בשנית. אם אינך מעוניין לשנות את בחירותייך, הקש אנטר\n").split()
+            if len(another_input):
+                correct_words = another_input
+                
+        return correct_words
+    
     def search(self):
         '''
         the search algorithm - first the user will choose the kind of recipe he's looking for
@@ -23,8 +54,9 @@ class Search(db):
         while usr_input not in range(1,8):
             usr_input = int(input("בחירה לא חוקית; אנא בחר שנית:\n1. ארוחות בוקר\n2. עיקריות\n3. קינוחים ומתוקים\n4. סלטים\n5. נשנושים וחטיפים\n6. שייקים ומשקאות\n7. גבינות וממרחים\n"))
         records = self.select(usr_input)
-        restrictions = input("מהן המגבלות התזונתיות? (ניתן לבחור יותר מאחד, יש להקיש רווח בין בחירה לבחירה)\n").split()
-        preferences = input("אילו מצרכים תרצה שיכללו במתכון? (ניתן לבחור יותר מאחד, יש להקיש רווח בין בחירה לבחירה)\n").split()
+        # checks for possible type errors in user input
+        restrictions = self.type_error(input("מהן המגבלות התזונתיות? (ניתן לבחור יותר מאחד, יש להקיש רווח בין בחירה לבחירה)\n").split())
+        preferences = self.type_error(input("אילו מצרכים תרצה שיכללו במתכון? (ניתן לבחור יותר מאחד, יש להקיש רווח בין בחירה לבחירה)\n").split())
         # all of the urls in the choosen category - for gluten option check
         urls = []
         # choosing recipe by input
@@ -42,7 +74,7 @@ class Search(db):
                 if url in urls:
                     if not any(x in ingr for x in restrictions) and all(x in ingr for x in preferences):
                         recipes[name] = url
-       # if no results were found, user will be able to start a new search
+        # if no results were found, user will be able to start a new search
         if recipes:
             # prints with empty line, separating the user input to the output, and from one recipe to the other
             print("")
@@ -51,29 +83,9 @@ class Search(db):
                 print(recipes[recipe])
                 print("")
         else:
-            # checks for typing errors of user's input
-            pref_and_rest = preferences + restrictions
-            i=1
-            while i<=1:
-                records = self.select(i)
-                ingr = [i.split() for i in list(list(zip(*records)))[2]]
-                ingr_flat = [item for sublist in ingr for item in sublist]
-                for word in pref_and_rest: 
-                    if word in ingr_flat:
-                        print(word)
-                        pref_and_rest.remove(word)
-                    if len(pref_and_rest) == 0:
-                        break
-                i += 1
-            # if a word is not found anywhere in the ingredients database, it may be a typing error
-            if len(pref_and_rest):
-                print("ייתכן והייתה שגיאה בקלט שהוזן. המילים הבאות לא נמצאו במאגרינו: ", end="")
-                print(*pref_and_rest)
-                another_search = input("להרצת חיפוש חדש, הקש 1 ולחץ אנטר\n")
-            else:
-                another_search = input("לא נמצאו מתכונים מתאימים. להרצת חיפוש חדש, הקש 1 ולחץ אנטר\n")
+            another_search = input("לא נמצאו מתכונים מתאימים. להרצת חיפוש חדש, הקש 1 ולחץ אנטר\n")
             if another_search == "1":
-                return self.search()   
+                return self.search()
 
 search = Search()
 search.search()
