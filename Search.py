3c0fe02a05bd19d598e9e94bc6a6a5b93f7b7813
MarvaZy <70692128+MarvaZy@users.noreply.github.com>
2020-10-19 13:50:53 +0300

    Update Search.py

--- Search.py
+++ Search.py
@@ -23,26 +23,24 @@ class Search(db):
         while usr_input not in range(1,8):
             usr_input = int(input("בחירה לא חוקית; אנא בחר שנית:\n1. ארוחות בוקר\n2. עיקריות\n3. קינוחים ומתוקים\n4. סלטים\n5. נשנושים וחטיפים\n6. שייקים ומשקאות\n7. גבינות וממרחים\n"))
         records = self.select(usr_input)
-        restrictions = input("מהן המגבלות התזונתיות? (ניתן לבחור יותר מאחד, יש להקיש רווח בין בחירה לבחירה)\n")
-        restrictions_list = restrictions.split()
-        preferences = input("אילו מצרכים תרצה שיכללו במתכון? (ניתן לבחור יותר מאחד, יש להקיש רווח בין בחירה לבחירה)\n")
-        preferences_list = preferences.split()
+        restrictions = input("מהן המגבלות התזונתיות? (ניתן לבחור יותר מאחד, יש להקיש רווח בין בחירה לבחירה)\n").split()
+        preferences = input("אילו מצרכים תרצה שיכללו במתכון? (ניתן לבחור יותר מאחד, יש להקיש רווח בין בחירה לבחירה)\n").split()
         # all of the urls in the choosen category - for gluten option check
         urls = []
         # choosing recipe by input
         for name, url, ingr in records:
             urls.append(url)
-            if not any(x in ingr for x in restrictions_list) and all(x in ingr for x in preferences_list):
+            if not any(x in ingr for x in restrictions) and all(x in ingr for x in preferences):
                 recipes[name] = url
 
         # if gluten is entered as restriction, algorithm will add the relevant recipes from the gluten cloumn in our table
-        if 'גלוטן' in restrictions_list:
+        if 'גלוטן' in restrictions:
             # no gluten recipes -> index = 8
             no_gluten = self.select(8) 
-            restrictions_list.remove('גלוטן')
+            restrictions.remove('גלוטן')
             for name, url, ingr in no_gluten:
                 if url in urls:
-                    if not any(x in ingr for x in restrictions_list) and all(x in ingr for x in preferences_list):
+                    if not any(x in ingr for x in restrictions) and all(x in ingr for x in preferences):
                         recipes[name] = url
         
         return recipes   
